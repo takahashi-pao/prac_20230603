8d33a393762c3ebd300b146c73eb5fe273b18d46
takahashi-pao <takahashi-pao>
2023-06-03 23:43:36 +0900

    hello handler

--- main.go
+++ main.go
@@ -9,10 +9,23 @@ var count int
 
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/greet", func(w http.ResponseWriter, r *http.Request) {
+		queryParams := r.URL.Query()
+		name := queryParams.Get("name")
+		handlerHello(w, r, name)
+	})
 	http.ListenAndServe(":8080", nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	count++
 	fmt.Fprintln(w, count)
+}
+
+func handlerHello(w http.ResponseWriter, r *http.Request, name string) {
+	/*
+		message := strings.Replace("Hello, {name}", "{name}", name, 1)
+	*/
+
+	fmt.Fprintf(w, "Hello, %s\n", name)
 }
